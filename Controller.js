d55735fece845a9dec33121ccff0927ae06f03f1
itbxh <beiller@gmail.com>
2015-11-24 17:01:57 -0500

    Update to use the FSM

--- Controller.js
+++ Controller.js
@@ -2,6 +2,95 @@
 var INVINCIBILITY_TIME = 2000;
 var BLEND_ANIMATION_TIME = 0.33;
 
+function Controllerv2(character, game) {
+    this.character = character;
+    this.game = game;
+    this.target = 'eve';
+    this.attackMovementSpeed = this.character.movementSpeed * 1.25;
+    this.nomalMovementSpeed = this.character.movementSpeed;
+    this.idleTillAttackTime = 1200;
+    this.idleAfterAttackTime = 3000;
+    this.viewDistance = 6.0;
+    this.attackDistance = 0.1;
+    this.damageDistance = this.character.mesh.geometry.boundingSphere.radius * 0.75;
+    this.stunDuration = 1000;
+    this.searchFrequency = 200; //look around every 200ms
+
+    this.update = function() {}
+
+    var controller = this;
+    var fsm = StateMachine.create({
+      initial: 'idle',
+      events: [
+        { name: 'activate',      from: 'idle',       to: 'search' },
+        { name: 'foundEnemy',    from: 'search',     to: 'approach'    },
+        { name: 'attack',        from: 'approach',   to: 'attacking' },
+        { name: 'cooldown',      from: 'attacking',     to: 'search'  }
+      ],
+      callbacks: {
+        onenteridle:  function(event, from, to, msg) {
+            console.log('enter idle state');
+            this.activate();
+        },
+        onentersearch:  function(event, from, to, msg) {
+            console.log('enter searching for player state');
+            character.setAnimation("DE_Boredom", { crossFade: true, crossFadeDuration: character.runBlendAnimationSpeed, crossFadeWarp: false });
+            //search for enemy
+            var fsm = this;
+            var interval = setInterval(function() {
+                console.log('searching for enemy');
+                var enemy = game.characters[controller.target];
+                if(enemy) {
+                    var dist = enemy.mesh.position.x - character.mesh.position.x;
+                    if(Math.abs(dist) < controller.viewDistance) {
+                        clearInterval(interval);
+                        fsm.foundEnemy();
+                    }
+                }
+            }, controller.searchFrequency);
+        },
+        onenterapproach:  function(event, from, to, msg) {
+            console.log('enter approaching player state');
+            character.setAnimation("DE_CombatRun", { crossFade: true, crossFadeDuration: character.runBlendAnimationSpeed, crossFadeWarp: false });
+            var fsm = this;
+            character.movementSpeed = character.attackMovementSpeed;
+            var intervalController = controller;
+            var interval = setInterval(function() {
+                console.log('pointing at enemy');
+                var dist = game.characters[intervalController.target].mesh.position.x - character.mesh.position.x;
+                if(Math.abs(dist) <= intervalController.attackDistance) {
+                    clearInterval(interval);
+                    fsm.attack();
+                }
+                character.movementDirection = new CANNON.Vec3(dist, 0, 0);
+                character.movementDirection.normalize();
+            }, 1000 / 60);
+            contoller.update = function() {
+                var forceVec = new CANNON.Vec3().copy(character.movementDirection);
+                forceVec.normalize();
+                var vLen = character.body.velocity.length();
+                if (vLen < character.movementSpeed) {
+                    character.body.applyForce(forceVec.scale(character.movementSpeed * 100.0), character.body.position);
+                }
+            }
+        },
+        onleaveapproach: function(event, from, to, msg) {
+            character.movementSpeed = normalMovementSpeed;
+        },
+        onenterattack:  function(event, from, to, msg) {
+            console.log('enter attacking state');
+            controller.update = function() {};
+            character.setAnimation("DE_Combatattack", {timeScale: 2.0});
+        },
+        onentercooldown:  function(event, from, to, msg) {
+            console.log('enter cooldown state');
+        }
+      }
+    });
+}
+//var cv2 = new Controllerv2();
+
+
 function Controller(character, game) {
     this.character = character;
     this.game = game;
